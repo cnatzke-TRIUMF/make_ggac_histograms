e0d03bcdac66663a0e8b081a4f49c5d37fc39efc
cnatzke <cnatzke@triumf.ca>
2020-07-22 10:19:54 -0700

    Initial Setup

---
+++ MakeGammaGammaHistograms.sh
@@ -0,0 +1,173 @@
+#!/bin/bash
+
+ANALDIR=/tig/belodon_data4/S9038/S9038-Nov2019/AnalysisTrees
+HISTDIR=/tig/belodon_data4/S9038/Projects/GammaGammaAnalysis/GenerateHistograms/myAnalysis/Histograms
+DLEN=${#ANALDIR}
+SORTCODE=/tig/belodon_data4/S9038/Projects/GammaGammaAnalysis/GenerateHistograms/myBuild/GammaGammaAnalysis
+
+# calibration files
+#CALFILE=/tig/belodon_data4/S9038/S9038-Nov2019/PreSrcCal.cal
+CALFILE=/tig/belodon_data4/S9038/S9038-Nov2019/PostSrcCal.cal
+#CFILE=/tig/belodon_data4/S9038/PostSrcCal.cal
+
+m=15600 #Only Sort after this run First good run: 52042
+n=99999 #Only Sort until this run
+BAD_RUN_LIST=(15719 15720)
+
+#############################################
+# Helper functions
+#############################################
+
+# checks if item is in list
+bad_run_check() {
+   [[ $1 =~ (^|[[:space:]])$2($|[[:space:]]) ]] && return 0 || return 1
+}
+
+#############################################
+# Main
+#############################################
+
+if [ $# -eq 0 ]
+then
+   echo "Pass 1 or 2 arguments"
+   echo "1: $0 run_number"
+   echo "   Process all subruns in run_number"
+   echo "2: $0 run_number batch_size"
+   echo "   Process all subruns in run_number in batches of batch_size"
+fi
+
+# for a single run
+if [ $# -eq 1 ]
+then
+    file=$ANALDIR/analysis"$1"_*.root;
+    g=${file:DLEN+9}
+    h=${g:0:${#g}-6}
+    i=${g:0:${#g}-7} # run number
+
+      RUNFILES=$ANALDIR/analysis${i}_"*".root
+      HFILE=$HISTDIR/gg_${i}.root
+
+      if [ $i -gt $m ] && [ $n -gt $i ];
+      then
+         if [ ! -f $HFILE ];
+         then
+           # echo "$SORTCODE $RUNFILES $CALFILE"
+           # echo "mv gg_* $HISTDIR/"
+           $SORTCODE $RUNFILES $CALFILE
+           mv gg_* $HISTDIR/gg_${i}_total.root
+
+         fi
+         if [ -f $HFILE ]
+         then
+            # if the sort code gets updated or the analysis tree is updated
+            if [ $HFILE -ot $SORTCODE ] || [ $HFILE -ot $f ];
+            then
+               echo "Sort code or analysis tree have changed, rerunning sort"
+                echo "$SORTCODE $RUNFILES $CALFILE"
+                echo "mv gg_* $HISTDIR/"
+               #$SORTCODE $RUNFILES $SELECTOR --max-workers=4
+               #mv TwoPhotonGriffinAnglePlots* $HISTDIR/
+            fi
+         fi
+     elif [[ $i < $m ]]; then
+         echo "Run $1 is less than the low run cutoff: $m"
+         echo "Exiting ..."
+     else
+         echo "$1 is non accepted run format"
+         echo "Exiting ..."
+      fi
+fi
+
+# for a range of subruns
+if [ $# -eq 2 ] # Run Number, Batch size
+then
+    declare -a FILE_BATCH
+    declare -a FILE_LIST
+    FILE_ITER=1
+    BATCH_COUNTER=0
+
+    RUNFILES=$ANALDIR/analysis${i}_"*".root
+    HFILE=$HISTDIR/gg_${i}.root
+
+    FILE_LIST=$(find ${ANALDIR}/ -name analysis"$1"_???.root | sort)
+    NUM_FOUND=$(find ${ANALDIR}/ -name analysis"$1"_???.root | wc -l)
+    TOTAL_BATCHES=$((NUM_FOUND / $2 + 1))
+    echo "Found ${NUM_FOUND} Files"
+    echo "Requires ${TOTAL_BATCHES} Batches"
+
+    for file in $FILE_LIST;
+    do
+
+        FILE_BATCH=("${FILE_BATCH[@]}" "$file")
+
+        # Process files in specified batch sizes
+        if (( $FILE_ITER % $2 == 0 ))
+        then
+            #echo "Files: ${FILE_BATCH[@]}"
+
+            if [ $1 -gt $m ] && [ $n -gt $1 ];
+            then
+                if [ ! -f $HFILE ];
+                then
+                    #echo "$SORTCODE ${FILE_BATCH[@]} $CALFILE"
+                    #echo "mv gg_* $HISTDIR/gg_$1_$BATCH_COUNTER.root"
+                    $SORTCODE ${FILE_BATCH[@]} $CALFILE
+                    mv gg_* $HISTDIR/gg_${1}_${BATCH_COUNTER}.root
+                fi
+                if [ -f $HFILE ]
+                then
+                   # if the sort code gets updated or the analysis tree is updated
+                    if [ $HFILE -ot $SORTCODE ] || [ $HFILE -ot $file ];
+                    then
+                        echo "Sort code or analysis tree have changed, rerunning sort"
+                        echo "$SORTCODE $FILE_BATCH $CALFILE"
+                        echo "mv gg_* $HISTDIR/gg_$1_$BATCH_COUNTER.root"
+                    fi
+                fi
+            fi
+
+            unset FILE_BATCH
+            BATCH_COUNTER=$((BATCH_COUNTER + 1))
+        fi
+
+        # Process remaining files
+        if (( $BATCH_COUNTER == $((TOTAL_BATCHES - 1))  && (( $FILE_ITER == $NUM_FOUND)) ))
+        then
+            echo "Files: ${FILE_BATCH[@]}"
+            if [ $1 -gt $m ] && [ $n -gt $1 ];
+            then
+                if [ ! -f $HFILE ];
+                then
+                    #echo "$SORTCODE ${FILE_BATCH[@]} $CALFILE"
+                    #echo "mv gg_* $HISTDIR/gg_$1_$BATCH_COUNTER.root"
+                    $SORTCODE ${FILE_BATCH[@]} $CALFILE
+                    mv gg_* $HISTDIR/gg_${i}_total.root
+                fi
+                if [ -f $HFILE ]
+                then
+                   # if the sort code gets updated or the analysis tree is updated
+                    if [ $HFILE -ot $SORTCODE ] || [ $HFILE -ot $file ];
+                    then
+                        echo "Sort code or analysis tree have changed, rerunning sort"
+                        echo "$SORTCODE $FILE_BATCH $CALFILE"
+                        echo "mv gg_* $HISTDIR/gg_$1_$BATCH_COUNTER.root"
+                    fi
+                fi
+            fi
+
+            unset FILE_BATCH
+        fi
+
+        FILE_ITER=$((FILE_ITER + 1))
+
+    done
+fi
+
+if [[ $# > 2 ]]
+then
+   echo "Pass 1 or 2 arguments"
+   echo "1: $0 run_number"
+   echo "   Process all subruns in run_number"
+   echo "2: $0 run_number batch_size"
+   echo "   Process all subruns in run_number in batches of batch_size"
+fi
---
+++ README.md
@@ -0,0 +1,69 @@
+# GammaGammaHistograms
+Creates gamma-gamma histograms from unpacked analysis trees. Time random background
+subtraction occurs while the histograms are being filled and a second event
+mixed (depth 10) matrix is created for normalization procedures.
+
+## Table of Contents
+  * [Installation](#installation)
+  * [Running GammaGammaHistograms](#running-gammagammahistograms)
+  * [Helper scripts](#helper-scripts)
+    + [MakeGammaGammaHistograms.sh](#makegammagammahistograms)
+
+# Installation
+0. Requires GRSISort 4.X.X
+1. Get the code, either via tarball or from github
+```
+git clone https://github.com/cnatzke/MakeGammaGammaHistograms.git
+```
+2. Build program using standard cmake process, e.g.
+```
+mkdir myBuild && cd myBuild
+cmake ..
+make
+```
+3. Do science.
+
+# Running MakeGammaGammaHistograms
+The general form of input is:
+```
+./GammaGammaHistograms analysis_tree [analysis_tree_2 ... ] calibration_file
+```
+
+##### Parameters
+```
+analysis_tree           ROOT file(s) containing analysis tree to process (must end with .root)
+calibration_file        GRIFFIN calibration file (must end with .cal)
+```
+
+##### Outputs
+```
+gg_XXXXX.root   ROOT file containing gammma gamma histograms
+```
+
+
+# Helper scripts
+Included is a helper script that makes building histograms easier.
+
+### MakeGammaGammaHistograms.sh
+This script takes a run and optionally a batch size.
+```
+./MakeGammaGammaHistograms.sh run_number batch_size
+```
+where
+```
+run_number  Run number of interest
+batch_size  Number of subruns to be chained together (optional)
+```
+The script chains subruns together to process, but depending on the size of the run  this exceeds the available system memory. The batch size controls how many subruns will be chained together in each step. For example if you have 17 subruns (0-16) and a batch size of 3, the script will pass a total of 6 batches to the histogram programs like so
+```
+batch   subruns
+---------------
+1       000-002
+2       003-005
+3       006-008
+4       009-011
+5       012-014
+6       015-016
+```
+
+If you do not pass a batch size the script will collects all subruns and pass them to the histogram sorting function. 
